9aae05414e0f0a0b181b18d0ca2dd02ed5906803
AMKRobinson <robinsonamk@gmail.com>
2021-01-27 09:23:32 -0800

    cleaned up some comments

--- scraper.py
+++ scraper.py
@@ -13,6 +13,7 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
 }
 
+# replace url with volume you need to download
 url = 'https://ko-fi.com/album/Spectral-Chaos-Vol-1--Basic-Lands-W7W834ARJ'
 
 r = requests.get(url, headers=headers)
@@ -21,23 +22,16 @@ print(r.status_code)
 
 splits = r.text.split("""onclick="viewImage('""")[1:]
 
-#splits[0].split("""'""")[0]
-
 hires_keys = []
 
 for item in splits:
     hires_keys.append(item.split("""'""")[0])
-    
-    
-#url for image
-    
-#https://ko-fi.com/Buttons/LoadGalleryItem?galleryItemId=  (IMAGE KEY)  &external=true&_=1611636437630
 
 for key in hires_keys:
     print('formatting url with custom key mapped out of hires_keys')
     coolurl = "https://ko-fi.com/Buttons/LoadGalleryItem?galleryItemId={}&external=true&_=1611636437630".format(key)
     
-    print('storing new get request in coolr containg new formatted url containing mapped key')
+    print('storing new get request in coolr containing new formatted url containing mapped key')
     coolr = requests.get(coolurl, headers=headers)
     
     print('extracting hires url out of coolr, splitting where necessary and storing it as hires_url')
